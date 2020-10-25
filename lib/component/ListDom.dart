739fb6c6654e296c94f4aefbc7b5b52567aa9bd0
chengczhang <zhangmvp123@vip.qq.com>
2020-10-25 18:05:41 +0800

    feat：'flutter环境搭建&首次提交'

---
+++ lib/component/ListDom.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+// var dataList =  ;
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'flutter Demo',
+        home: Scaffold(
+            appBar: new AppBar(title: new Text('北京 - 上海')),
+            body: new ListView(
+              children: <Widget>[
+                new ListTile(
+                  leading: new Icon(Icons.flight_takeoff),
+                  title: new Text(
+                    '04:15 - 08:40             ￥500',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                  subtitle: new Text(
+                    '大兴国际        浦东T1',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  contentPadding: EdgeInsets.all(20),
+                ),
+                new ListTile(
+                  leading: new Icon(Icons.flight_takeoff),
+                  title: new Text(
+                    '04:15 - 08:40             ￥500',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                  subtitle: new Text(
+                    '大兴国际        浦东T1',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  contentPadding: EdgeInsets.all(20),
+                ),
+                new ListTile(
+                  leading: new Icon(Icons.flight_takeoff),
+                  title: new Text(
+                    '04:15 - 08:40             ￥500',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                  subtitle: new Text(
+                    '大兴国际        浦东T1',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  contentPadding: EdgeInsets.all(20),
+                ),
+                new ListTile(
+                  leading: new Icon(Icons.flight_takeoff),
+                  title: new Text(
+                    '04:15 - 08:40             ￥500',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                  subtitle: new Text(
+                    '大兴国际        浦东T1',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  contentPadding: EdgeInsets.all(20),
+                ),
+                new ListTile(
+                  leading: new Icon(Icons.flight_takeoff),
+                  title: new Text(
+                    '04:15 - 08:40             ￥500',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                  subtitle: new Text(
+                    '大兴国际        浦东T1',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  contentPadding: EdgeInsets.all(20),
+                ),
+                new ListTile(
+                  leading: new Icon(Icons.flight_takeoff),
+                  title: new Text(
+                    '04:15 - 08:40             ￥500',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                  subtitle: new Text(
+                    '大兴国际        浦东T1',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  contentPadding: EdgeInsets.all(20),
+                ),
+                new ListTile(
+                  leading: new Icon(Icons.flight_takeoff),
+                  title: new Text(
+                    '04:15 - 08:40             ￥500',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                  subtitle: new Text(
+                    '大兴国际        浦东T1',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  contentPadding: EdgeInsets.all(20),
+                ),
+                new ListTile(
+                  leading: new Icon(Icons.flight_takeoff),
+                  title: new Text(
+                    '04:15 - 08:40             ￥500',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                  subtitle: new Text(
+                    '大兴国际        浦东T1',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  contentPadding: EdgeInsets.all(20),
+                ),
+                new ListTile(
+                  leading: new Icon(Icons.flight_takeoff),
+                  title: new Text(
+                    '04:15 - 08:40             ￥500',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                  subtitle: new Text(
+                    '大兴国际        浦东T1',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  contentPadding: EdgeInsets.all(20),
+                ),
+                new ListTile(
+                  leading: new Icon(Icons.flight_takeoff),
+                  title: new Text(
+                    '04:15 - 08:40             ￥500',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                  subtitle: new Text(
+                    '大兴国际        浦东T1',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  contentPadding: EdgeInsets.all(20),
+                ),
+                new ListTile(
+                  leading: new Icon(Icons.flight_takeoff),
+                  title: new Text(
+                    '04:15 - 08:40             ￥500',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                  subtitle: new Text(
+                    '大兴国际        浦东T1',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  contentPadding: EdgeInsets.all(20),
+                ),
+                new ListTile(
+                  leading: new Icon(Icons.flight_takeoff),
+                  title: new Text(
+                    '04:15 - 08:40             ￥500',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                  subtitle: new Text(
+                    '大兴国际        浦东T1',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  contentPadding: EdgeInsets.all(20),
+                ),
+                new ListTile(
+                  leading: new Icon(Icons.flight_takeoff),
+                  title: new Text(
+                    '04:15 - 08:40             ￥500',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                  subtitle: new Text(
+                    '大兴国际        浦东T1',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  contentPadding: EdgeInsets.all(20),
+                ),
+              ],
+            )));
+  }
+}
